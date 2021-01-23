09d658bf561c877242ee97e902fe97d41f8a1dbd
ZenithNUC <lizengzhi1998@foxmail.com>
2021-01-23 22:02:19 +0800

    feat:增加了构造参数的获取函数

--- pic/addWaterMark.py
+++ pic/addWaterMark.py
@@ -14,3 +14,11 @@ def create_parser():
     parser.add_argument('-r',dest='resImage',required=True)             # 输出结果
     parser.add_argument('--a',dest='alphaValue',default=ALPHA)          # ALPHA值
     return parser
+
+def option_value():
+    parser = create_parser()
+    options = parser.parse_args()
+    sImage = options.sourceImage
+    wImage = options.wmImage
+    rImage = options.resImage
+    return sImage,wImage,rImage
